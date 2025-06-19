09d345e1f1a3ab3d7b4231fcbb0de1fdffe8448c
noelmarior <noelroche06@gmail.com>
2025-06-19 20:11:02 +0530

    Updated sorting-logic to latest version

--- taskTwo.java
+++ taskTwo.java
@@ -2,12 +2,20 @@ import java.util.*;
 
 public class taskTwo {
     public static void main(String[] args ){
+           int[] arr = getElement();
+           sortArray(arr);
+           printVal(arr);
+    }
+    public static int[] getElement(){
         Scanner sc = new Scanner(System.in);
         int arr[]=new int[5];
         System.out.print("Enter Five elements: ");
         for (int i =0; i<arr.length; i++){
             arr[i]=sc.nextInt();
         }
+        return arr;
+    }
+    public static void sortArray(int[] arr){
         for (int i=0; i<arr.length-1; i++){
             int small=i;
             for (int j=i+1;j<arr.length;j++){
@@ -21,7 +29,8 @@ public class taskTwo {
                 arr[small]=temp;
             }
         }
-        
+    }
+    public static void printVal(int[] arr){
         System.out.println("Second Lowest: "+arr[1]);
         System.out.println("Second Highest: "+arr[arr.length-2]);
     }
